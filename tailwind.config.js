0f6aee96ca588275b25a86cf741d39b2d759ea7f
Nayem Khan <nayemk900@gmail.com>
2022-11-05 02:03:18 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    content: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.tsx', './src/**/*.js']
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
