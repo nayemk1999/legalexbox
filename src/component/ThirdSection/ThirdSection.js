3c8c831e666ad4fe72bb5213ec1c71037eb394fe
alhabib911 <abdullahalhabib100@gmail.com>
2022-11-05 23:56:27 +0600

    third section added

--- src/component/ThirdSection/ThirdSection.js
+++ src/component/ThirdSection/ThirdSection.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const ThirdSection = () => {
     return (
-        <div>
-            <div>
-
+        <div className='flex mx-28 pt-24'>
+            <div className='bg-gradient-to-r from-[#132238] to-[#03142D] rounded-3xl w-3/5'>
+                <img src="/img-content-graph.png" className='w-11/12' alt="" />
             </div>
-            <div>
-                <img src="/img-content-graph.png" className='w-6/12' alt="" />
+            <div className='w-2/5'>
+habob   
             </div>
         </div>
     );
