5619c294942f5a87a9c17200f0a5d0500216035a
Nayem Khan <nayemk900@gmail.com>
2022-11-05 23:52:08 +0600

    fixed issue

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="w-[100vh] bg-red-500">
+      <p className="text-white">Error Page.........</p>
+    </div>
+  );
+};
+
+export default Error;
